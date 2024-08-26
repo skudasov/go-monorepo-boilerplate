d7cb7682c791faae5381fbff52b21fc5d8d547fb
skudasov <f4hrenh9it@gmail.com>
2024-08-26 21:52:05 +0200

    simple change for three

--- three/cmd/three.go
+++ three/cmd/three.go
@@ -3,12 +3,5 @@ package main
 import "github.com/skudasov/go-monorepo-boilerplate/three"
 
 func main() {
-	// some minor changes
-	// more minor changes
-	// major changes
-	// more changes
-	// more changes again
-	// and again
-	// more changes
 	three.Three(1, 2)
 }
