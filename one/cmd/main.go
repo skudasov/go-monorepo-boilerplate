41a84de1121b3cc61218b0f2420513366b565a87
skudasov <f4hrenh9it@gmail.com>
2024-08-26 12:23:19 +0200

    try release action

---
+++ one/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/skudasov/go-monorepo-boilerplate/one"
+
+func main() {
+	one.One(1, 2)
+}
---
+++ two/cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/skudasov/go-monorepo-boilerplate/two"
+)
+
+func main() {
+	two.Two(1, 2)
+}
