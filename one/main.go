6c938f66c3896161a32671db27d964f04582bcde
skudasov <f4hrenh9it@gmail.com>
2024-08-26 11:13:47 +0200

    multiple releases

--- one/main.go
+++ one/main.go
@@ -5,6 +5,7 @@ import "github.com/davecgh/go-spew/spew"
 func One(a, b int) int {
 	r := a + b
 	// release #1 tag v1.0.0
+	// release v1.1.0
 	spew.Dump(r)
 	return r
 }
--- two/main.go
+++ two/main.go
@@ -4,6 +4,7 @@ import "github.com/davecgh/go-spew/spew"
 
 func Two(a, b int) int {
 	r := a + b
+	// release v1.0.0
 	spew.Dump(r)
 	return r
 }
