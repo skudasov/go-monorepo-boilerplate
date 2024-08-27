81738bf866a3a5a0fd9500b3ad74640a4a305ed4
skudasov <f4hrenh9it@gmail.com>
2024-08-27 12:12:03 +0200

    breaking change in one

--- one/main.go
+++ one/main.go
@@ -2,13 +2,8 @@ package one
 
 import "github.com/davecgh/go-spew/spew"
 
-func One(a, b, c int) int {
-	r := a + b + c
-	// release #1 tag v1.0.0
-	// release v1.1.0
-	// release v1.2.0
-	// release v1.3.0
-	// another change
+func One(a, b int) int {
+	r := a + b
 	spew.Dump(r)
 	return r
 }
